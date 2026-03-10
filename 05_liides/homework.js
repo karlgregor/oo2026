98ee7e0157fd863132baa888a1cf4f0fda40b50c
Karl-Gregor Keerles <keerleskarlgregor@gmail.com>
2026-03-10 18:28:05 +0000

    Week 05: Homework

---
+++ 05_liides/homework.js
@@ -0,0 +1,42 @@
+var CelsiusToFahrenheit = /** @class */ (function () {
+    function CelsiusToFahrenheit() {
+    }
+    CelsiusToFahrenheit.prototype.calculate = function (x) {
+        return Math.round((x * 9 / 5 + 32) * 10) / 10;
+    };
+    CelsiusToFahrenheit.prototype.inputUnit = function () {
+        return "°C";
+    };
+    CelsiusToFahrenheit.prototype.outputUnit = function () {
+        return "°F";
+    };
+    return CelsiusToFahrenheit;
+}());
+var FahrenheitToCelsius = /** @class */ (function () {
+    function FahrenheitToCelsius() {
+    }
+    FahrenheitToCelsius.prototype.calculate = function (x) {
+        return Math.round(((x - 32) * 5 / 9) * 10) / 10;
+    };
+    FahrenheitToCelsius.prototype.inputUnit = function () {
+        return "°F";
+    };
+    FahrenheitToCelsius.prototype.outputUnit = function () {
+        return "°C";
+    };
+    return FahrenheitToCelsius;
+}());
+var CelsiusToKelvin = /** @class */ (function () {
+    function CelsiusToKelvin() {
+    }
+    CelsiusToKelvin.prototype.calculate = function (x) {
+        return Math.round((x + 273.15) * 10) / 10;
+    };
+    CelsiusToKelvin.prototype.inputUnit = function () {
+        return "°C";
+    };
+    CelsiusToKelvin.prototype.outputUnit = function () {
+        return "K";
+    };
+    return CelsiusToKelvin;
+}());
---
+++ 05_liides/homework.ts
@@ -0,0 +1,47 @@
+interface CalculatingFunction {
+    calculate(x: number): number;
+    inputUnit(): string;
+    outputUnit(): string;
+}
+
+class CelsiusToFahrenheit implements CalculatingFunction {
+    calculate(x: number): number {
+        return Math.round((x * 9/5 + 32) * 10) / 10;
+    }
+
+    inputUnit(): string {
+        return "°C";
+    }
+
+    outputUnit(): string {
+        return "°F";
+    }
+}
+
+class FahrenheitToCelsius implements CalculatingFunction {
+    calculate(x: number): number {
+        return Math.round(((x - 32) * 5/9) * 10) / 10;
+    }
+
+    inputUnit(): string {
+        return "°F";
+    }
+
+    outputUnit(): string {
+        return "°C";
+    }
+}
+
+class CelsiusToKelvin implements CalculatingFunction {
+    calculate(x: number): number {
+        return Math.round((x + 273.15) * 10) / 10;
+    }
+
+    inputUnit(): string {
+        return "°C";
+    }
+
+    outputUnit(): string {
+        return "K";
+    }
+}
